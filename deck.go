6e1e1324b9c7f5f7a850f10e638218cc5910cffe
SebastiaanHulst <sebastiaanhulst@hotmail.com>
2018-03-21 12:42:50 +0100

    Added deck of cards

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -46,4 +47,11 @@ func (d deck) saveToFile(filename string) error {
 
 func newDeckFromFile(filename string) deck {
 	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
